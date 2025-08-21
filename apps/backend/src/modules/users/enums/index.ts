2722d67d459d7ed35666c62148d999b1897b4721
OtavioAL <otavioauglei@gmail.com>
2025-08-21 11:28:30 -0300

    feat(backend/enums): create user-related enums

---
+++ apps/backend/src/modules/users/enums/index.ts
@@ -0,0 +1,19 @@
+export enum UserRole {
+  ADMIN = 'admin',
+  TRAINER = 'trainer',
+  STUDENT = 'student',
+}
+
+export enum UserStatus {
+  ACTIVE = 'active',
+  INACTIVE = 'inactive',
+}
+
+export enum BmiClassificationEnum {
+  UNDERWEIGHT = 'underweight',
+  NORMAL_WEIGHT = 'normal_weight',
+  OVERWEIGHT = 'overweight',
+  OBESITY_CLASS_1 = 'obesity_class_1',
+  OBESITY_CLASS_2 = 'obesity_class_2',
+  OBESITY_CLASS_3 = 'obesity_class_3',
+}
