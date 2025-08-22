3b06a29f4f620b914a0c285111160eadc7b5aa72
OtavioAL <otavioauglei@gmail.com>
2025-08-21 23:06:21 -0300

    feat: create AuthenticateService

---
+++ apps/backend/src/modules/tokens/services/AuthenticateService.ts
@@ -0,0 +1,45 @@
+import { IUsersRepository } from '../../users/repositories/IUsersRepository';
+import { AppError } from '../../../shared/errors/AppError';
+import bcrypt from 'bcryptjs';
+import { env } from '../../../config/env';
+import { UserTokensRepository } from '../repositories/implementations/UserTokensRepository';
+import { INACTIVE_USER, INVALID_CREDENTIALS } from 'apps/backend/src/shared/errors/error.messages';
+import { UserStatus } from '../../users/enums';
+import { generateRefreshToken, generateToken } from 'apps/backend/src/utils/jwt';
+import { parseTimeToMilliseconds } from 'apps/backend/src/utils/parse-time';
+
+export class AuthenticateService {
+  constructor(
+    private usersRepo: IUsersRepository,
+    private tokensRepo: UserTokensRepository,
+  ) {}
+
+  async execute(username: string, password: string) {
+    const user = await this.usersRepo.findByUsername(username);
+
+    if (!user) throw new AppError(INVALID_CREDENTIALS, 401);
+
+    if (user.status === UserStatus.INACTIVE) throw new AppError(INACTIVE_USER, 403);
+    const doesPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!doesPasswordMatch) throw new AppError(INVALID_CREDENTIALS, 401);
+
+    const payloadToken = {
+      sub: user.id,
+      role: user.role,
+    };
+
+    const accessToken = generateToken(payloadToken);
+
+    const refreshToken = generateRefreshToken(payloadToken);
+
+    const expiresAt = new Date(Date.now() + parseTimeToMilliseconds(env.JWT_REFRESH_EXPIRES_IN));
+
+    await this.tokensRepo.createRefresh(user, refreshToken, expiresAt);
+
+    return {
+      accessToken,
+      refreshToken,
+      user: { id: user?.id, name: user.name, role: user.role },
+    };
+  }
+}
