9e2106d959ed6e860d66d3a6365ac787bf42c54f
OtavioAL <otavioauglei@gmail.com>
2025-08-21 23:07:14 -0300

    feat: create ToggleStatusService

---
+++ apps/backend/src/modules/users/services/ToggleStatusService.ts
@@ -0,0 +1,18 @@
+import { IUsersRepository } from '../repositories/IUsersRepository';
+import { AppError } from '../../../shared/errors/AppError';
+import { UserRole, UserStatus } from '../enums';
+import { UNAUTHORIZED, USER_NOT_FOUND } from 'apps/backend/src/shared/errors/error.messages';
+
+export class ToggleStatusService {
+  constructor(private usersRepo: IUsersRepository) {}
+  async execute(id: string, currentUserRole: UserRole) {
+    if (currentUserRole !== UserRole.ADMIN) throw new AppError(UNAUTHORIZED, 403);
+    const user = await this.usersRepo.findById(id);
+
+    if (!user) throw new AppError(USER_NOT_FOUND, 404);
+
+    user.status = user.status === UserStatus.ACTIVE ? UserStatus.INACTIVE : UserStatus.ACTIVE;
+
+    return this.usersRepo.save(user);
+  }
+}
