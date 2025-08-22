9c3d0a3ecd5bb59a93f4dc315184198fdbf18fa6
OtavioAL <otavioauglei@gmail.com>
2025-08-21 23:09:40 -0300

    feat: create validations/update-users.ts - validation for updating users

---
+++ packages/shared/src/validations/update-user.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+export const updateUserSchema = z.object({
+  username: z.string().min(3).optional(),
+  password: z.string().min(6).optional(),
+  role: z.enum(['admin', 'trainer', 'student']).optional(),
+  status: z.enum(['active', 'inactive']).optional(),
+});
+
+export type UpdateUserInput = z.infer<typeof updateUserSchema>;
