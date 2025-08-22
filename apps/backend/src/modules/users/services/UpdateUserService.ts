69b1e8aa94aaeab28e6cc602751067de71a191f6
OtavioAL <otavioauglei@gmail.com>
2025-08-21 23:07:22 -0300

    feat: create UpdateUserService

---
+++ apps/backend/src/modules/users/services/UpdateUserService.ts
@@ -0,0 +1,28 @@
+import { UpdateUserInput } from '@shared/validations/update-user';
+import { IUsersRepository } from '../repositories/IUsersRepository';
+import bcrypt from 'bcryptjs';
+import { UserRole, UserStatus } from '../enums';
+import { AppError } from 'apps/backend/src/shared/errors/AppError';
+import { UNAUTHORIZED, USER_NOT_FOUND } from 'apps/backend/src/shared/errors/error.messages';
+
+export class UpdateUserService {
+  constructor(private usersRepo: IUsersRepository) {}
+  async execute(id: string, data: UpdateUserInput, currentUserRole: UserRole) {
+    if (currentUserRole === UserRole.STUDENT) throw new AppError(UNAUTHORIZED, 403);
+    if (currentUserRole === UserRole.TRAINER && data?.role && data?.role !== UserRole.STUDENT) {
+      throw new AppError(UNAUTHORIZED, 403);
+    }
+    const { username, password, role, status } = data;
+
+    const user = await this.usersRepo.findById(id);
+    if (!user) throw new AppError(USER_NOT_FOUND, 404);
+
+    if (username) user.username = username;
+    if (role) user.role = UserRole[role.toUpperCase() as keyof typeof UserRole];
+
+    if (status) user.status = UserStatus[status.toUpperCase() as keyof typeof UserStatus];
+    if (password) user.password = await bcrypt.hash(password, 10);
+
+    return this.usersRepo.save(user);
+  }
+}
