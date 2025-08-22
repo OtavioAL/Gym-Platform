24a90f841d061d0fc099bc7ac1110e9282704cd3
OtavioAL <otavioauglei@gmail.com>
2025-08-21 23:06:38 -0300

    feat: create IUsersRepository interface

---
+++ apps/backend/src/modules/users/repositories/IUsersRepository.ts
@@ -0,0 +1,10 @@
+import { User } from '../entities/User';
+
+export interface IUsersRepository {
+  create(data: Partial<User>): Promise<User>;
+  save(user: User): Promise<User>;
+  findById(id: string): Promise<User | null>;
+  findByUsername(username: string): Promise<User | null>;
+  hasEvaluations(id: string): Promise<boolean>;
+  delete(id: string): Promise<void>;
+}
