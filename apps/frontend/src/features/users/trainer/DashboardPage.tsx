a0d771ec098c188bc8862f551adc48e6a73cbdda
OtavioAL <otavioauglei@gmail.com>
2025-08-27 01:03:24 -0300

    refactor(trainer): update redirect logic in TrainerDashboardPage

--- apps/frontend/src/features/users/trainer/DashboardPage.tsx
+++ apps/frontend/src/features/users/trainer/DashboardPage.tsx
@@ -4,12 +4,12 @@ import DashboardCard from '../components/DashboardCard';
 const TrainerDashboardPage = () => {
   return (
     <SimpleGrid columns={[1, 2, 3]} spacing={6}>
-      <DashboardCard title="Turmas" desc="Gerencie alunos e turmas." link="/admin/users" />
-      <DashboardCard title="Avaliações" desc="Crie e revise avaliações." link="/admin/users" />
+      <DashboardCard title="Turmas" desc="Gerencie alunos e turmas." link="/trainer/users" />
+      <DashboardCard title="Avaliações" desc="Crie e revise avaliações." link="/trainer/users" />
       <DashboardCard
         title="Relatórios"
         desc="Acompanhe o desempenho dos alunos."
-        link="/admin/users"
+        link="/admin/users/evaluations"
       />
     </SimpleGrid>
   );
