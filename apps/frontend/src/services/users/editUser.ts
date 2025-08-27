f8fadbbd26374802038f7dd516b2ad938d3735c9
OtavioAL <otavioauglei@gmail.com>
2025-08-26 22:44:46 -0300

    feat(users): create editUser service

---
+++ apps/frontend/src/services/users/editUser.ts
@@ -0,0 +1,7 @@
+import { UpdateUserInput } from '../../../../../packages/shared/src/validations/update-user';
+import api from '../../lib/api';
+
+export async function editUser({ username, password, role, id }: UpdateUserInput & { id: string }) {
+  const { data } = await api.put(`/users/${id}`, { name, username, password, role });
+  return data;
+}
