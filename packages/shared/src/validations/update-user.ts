6ac49421ec6d40fa64c1d682e7d46217091264ef
OtavioAL <otavioauglei@gmail.com>
2025-08-27 01:03:49 -0300

    feat(validations): allow name field in updateUserSchema

--- packages/shared/src/validations/update-user.ts
+++ packages/shared/src/validations/update-user.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 
 export const updateUserSchema = z.object({
+  name: z.string().min(3).optional(),
   username: z.string().min(3).optional(),
   password: z.string().min(6).optional(),
   role: z.enum(['admin', 'trainer', 'student']).optional(),
