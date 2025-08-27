b5a8bbbba436bababf945b60a4b25d9928298393
OtavioAL <otavioauglei@gmail.com>
2025-08-27 01:00:01 -0300

    feat(dashboard): render StudentDashboardPage for logged student

--- apps/frontend/src/app/dashboard/page.tsx
+++ apps/frontend/src/app/dashboard/page.tsx
@@ -5,20 +5,26 @@ import { useAuth } from '../../context/AuthContext';
 import Header from '../../components/Header';
 import AdminDashboardPage from '@/features/users/admin/DashboardPage';
 import TrainerDashboardPage from '@/features/users/trainer/DashboardPage';
-import { LoadingSpinner } from '@/components/LoadingSpinner';
+import StudentDashboardPage from '@/features/users/student/DashboardPage';
 
 export default function Dashboard() {
   const { user } = useAuth();
+
+  const TYPE_USER = {
+    admin: 'Administrador',
+    trainer: 'Professor',
+    student: 'Aluno',
+  };
 
   return (
     <>
       <Header />
       <Box p={8}>
         <Heading mb={6}>Bem-vindo, {user?.name}</Heading>
-        <Text mb={8}>Você está logado como: {user?.role?.toUpperCase()}</Text>
+        <Text mb={8}>Você está logado como: {TYPE_USER[user?.role as keyof typeof TYPE_USER]}</Text>
 
         {user?.role === 'admin' && <AdminDashboardPage />}
-        {/* {user?.role === 'student' && <TrainerDashboardPage />} */}
+        {user?.role === 'student' && <StudentDashboardPage />}
         {user?.role === 'trainer' && <TrainerDashboardPage />}
       </Box>
     </>
