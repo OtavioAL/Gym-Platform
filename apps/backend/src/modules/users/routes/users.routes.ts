ade13f478ba7190dda5fe3b08d6a37d83d7cc76b
OtavioAL <otavioauglei@gmail.com>
2025-08-26 22:30:30 -0300

    refactor(users): update users routes and add /me, get(/), and get(:id) routes

--- apps/backend/src/modules/users/routes/users.routes.ts
+++ apps/backend/src/modules/users/routes/users.routes.ts
@@ -2,22 +2,42 @@ import { Router, type Router as ExpressRouter } from 'express';
 import { ensureAuthenticated } from 'apps/backend/src/shared/middlewares/ensureAuthenticated';
 import { UsersController } from '../controllers/UsersController';
 import { ensureRole } from 'apps/backend/src/shared/middlewares/ensureRole';
+import { UserRole } from '../enums';
 
 const userRoutes: ExpressRouter = Router();
 
-const userControlle = new UsersController();
+const userController = new UsersController();
 
-userRoutes.post('/users', ensureAuthenticated, ensureRole('admin', 'trainer'), (req, res) =>
-  userControlle.create(req, res),
+userRoutes.post(
+  '/',
+  ensureAuthenticated,
+  ensureRole(UserRole.ADMIN, UserRole.TRAINER),
+  (req, res) => userController.create(req, res),
 );
-userRoutes.put('/users/:id', ensureAuthenticated, ensureRole('admin', 'trainer'), (req, res) =>
-  userControlle.update(req, res),
+userRoutes.put(
+  '/:id',
+  ensureAuthenticated,
+  ensureRole(UserRole.ADMIN, UserRole.TRAINER),
+  (req, res) => userController.update(req, res),
 );
-userRoutes.patch('/users/:id/status', ensureAuthenticated, ensureRole('admin'), (req, res) =>
-  userControlle.toggle(req, res),
+userRoutes.patch('/:id/status', ensureAuthenticated, ensureRole(UserRole.ADMIN), (req, res) =>
+  userController.toggle(req, res),
 );
-userRoutes.delete('/users/:id', ensureAuthenticated, ensureRole('admin'), (req, res) =>
-  userControlle.remove(req, res),
+userRoutes.delete('/:id', ensureAuthenticated, ensureRole(UserRole.ADMIN), (req, res) =>
+  userController.remove(req, res),
+);
+
+userRoutes.get('/me', ensureAuthenticated, (req, res) => userController.me(req, res));
+
+userRoutes.get('/', ensureAuthenticated, ensureRole(UserRole.ADMIN, UserRole.TRAINER), (req, res) =>
+  userController.list(req, res),
+);
+
+userRoutes.get(
+  '/:id',
+  ensureAuthenticated,
+  ensureRole(UserRole.ADMIN, UserRole.TRAINER),
+  (req, res) => userController.get(req, res),
 );
 
 export { userRoutes };
