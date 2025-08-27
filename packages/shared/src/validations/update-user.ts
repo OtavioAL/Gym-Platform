6163eeed96883c66c9c85497cfd9508b244ed547
OtavioAL <otavioauglei@gmail.com>
2025-08-27 08:41:11 -0300

    fix: update Zod validation messages for better clarity and accuracy

--- packages/shared/src/validations/update-user.ts
+++ packages/shared/src/validations/update-user.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 
 export const updateUserSchema = z.object({
-  name: z.string().min(3).optional(),
-  username: z.string().min(3).optional(),
-  password: z.string().min(6).optional(),
+  name: z.string().min(3, { message: 'Nome inválido' }).optional(),
+  username: z.string().min(3, { message: 'Email inválido' }).optional(),
+  password: z.string().min(6, { message: 'Senha inválida' }).optional(),
   role: z.enum(['admin', 'trainer', 'student']).optional(),
   status: z.enum(['active', 'inactive']).optional(),
 });
