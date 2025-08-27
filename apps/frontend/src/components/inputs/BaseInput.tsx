25faa4c20c2c3233f3c8bcd617fbe24d40d36222
OtavioAL <otavioauglei@gmail.com>
2025-08-27 08:40:53 -0300

    fix: correct height value formatting from currency to decimal representation

--- apps/frontend/src/components/inputs/BaseInput.tsx
+++ apps/frontend/src/components/inputs/BaseInput.tsx
@@ -1,21 +1,15 @@
 'use client';
 
 import { FormControl, FormErrorMessage, FormLabel, Input, InputProps } from '@chakra-ui/react';
-import { useFormContext } from 'react-hook-form';
 
 type BaseInputProps = {
   name: string;
   label: string;
   register: any;
+  error: any;
 } & InputProps;
 
-export const BaseInput = ({ name, label, register, ...rest }: BaseInputProps) => {
-  const {
-    formState: { errors },
-  } = useFormContext();
-
-  const error = errors[name]?.message as string | undefined;
-
+export const BaseInput = ({ name, label, register, error, ...rest }: BaseInputProps) => {
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={name}>{label}</FormLabel>
